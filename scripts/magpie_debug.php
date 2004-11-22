dfb8c8009ce09a2b7b7329a9f3015384ae3bfca1
kellan <kellan>
2004-11-22 07:11:15 +0000

    last bit of tidying

--- scripts/magpie_debug.php
+++ scripts/magpie_debug.php
@@ -1,31 +1,41 @@
 <?php
 
-#echo "Warnings and Notices<p>";
-#ini_set('display_errors', 1);
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
 
-// define path to Magpie files
-// and load library
-define('MAGPIE_DIR', '/home/kellan/projs/magpierss/');
-require_once(MAGPIE_DIR.'rss_fetch.inc');
-
+define('MAGPIE_DIR', '../');
 define('MAGPIE_DEBUG', 2);
 // flush cache quickly for debugging purposes, don't do this on a live site
 define('MAGPIE_CACHE_AGE', 2);
 
+require_once(MAGPIE_DIR.'rss_fetch.inc');
 
-$url = "http://localhost/rss/laughingmeme.rdf";
+$url = $_GET['url'];
 
-$rss = fetch_rss( $url );
-
-echo "</p><p>RSS Information</p>";
-echo "<p><b>Displaying RSS:</b> $url";
-
-if ( !$rss ) {
-	echo ("Error: " . magpie_error() );
+if ( $url ) {
+	$rss = fetch_rss( $url );
+	
+	if ($rss) {
+		echo "Channel: " . $rss->channel['title'] . "<p>";
+		echo "<ul>";
+		foreach ($rss->items as $item) {
+			$href = $item['link'];
+			$title = $item['title'];	
+			echo "<li><a href=$href>$title</a></li>";
+		}
+		echo "</ul>";
+	}
+	else {
+		echo "Error: " . magpie_error();
+	}
 }
-else {
-	$rss->show_channel();
-	$rss->show_list();
-}
-
 ?>
+
+<form>
+	RSS URL: <input type="text" size="30" name="url" value="<?php echo $url ?>"><br />
+	<input type="submit" value="Parse RSS">
+</form>
+
+<pre>
+<?php var_dump($rss); ?>
+</pre>
